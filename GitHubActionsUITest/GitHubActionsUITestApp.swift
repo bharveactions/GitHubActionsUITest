d817d762c44342a9efdfdffca5391464c62340e7
bharve12 <brian.harvey@kub.org>
2023-03-23 08:50:17 -0400

    Initial Commit

---
+++ GitHubActionsUITest/GitHubActionsUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsUITestApp.swift
+//  GitHubActionsUITest
+//
+//  Created by admin on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
